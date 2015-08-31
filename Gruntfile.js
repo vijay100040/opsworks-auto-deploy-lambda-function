e7cc33aa547f22ad18b02287c491e65d6d3362e5
Vijayakumar K <vijay@bambeeq.com>
2015-08-31 11:23:39 +0530

    Improved code consistency w.r.t formatting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,48 +1,48 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  grunt.initConfig({
-    jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-      options: {
-        globals: {
-          jQuery: true
-        }
-      }
-    },
-    lambda_invoke: {
-            default: {
-                options: {
-                    // Task-specific options go here.
-                    "profile":process.env.AWS_PROFILE,
-                    "region":process.env.AWS_REGION,
-                    "event": "event.json"
-                }
-            }
-    },
-    lambda_package: {
-            default: {
-                options: {
-                    // Task-specific options go here.
-                    "profile":process.env.AWS_PROFILE,
-                    "region":process.env.AWS_REGION,
-                }
-            }
-    },
-    lambda_deploy: {
-          default: {
-              options: {
-                  // Task-specific options go here.
-                  "profile":process.env.AWS_PROFILE,
-                  "region":process.env.AWS_REGION,
-              }
-              ,"arn":process.env.LAMBDA_FN_ARN
-          }
-      }
-  });
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-aws-lambda');
+	grunt.initConfig({
+		jshint: {
+			files: ["Gruntfile.js", "src/**/*.js", "test/**/*.js"],
+			options: {
+				globals: {
+					jQuery: true
+				}
+			}
+		},
+		lambda_invoke: {
+			default: {
+				options: {
+					// Task-specific options go here.
+					"profile": process.env.AWS_PROFILE,
+					"region": process.env.AWS_REGION,
+					"event": "event.json"
+				}
+			}
+		},
+		lambda_package: {
+			default: {
+				options: {
+					// Task-specific options go here.
+					"profile": process.env.AWS_PROFILE,
+					"region": process.env.AWS_REGION
+				}
+			}
+		},
+		lambda_deploy: {
+			default: {
+				options: {
+					// Task-specific options go here.
+					"profile": process.env.AWS_PROFILE,
+					"region": process.env.AWS_REGION
+				},
+				"arn": process.env.LAMBDA_FN_ARN
+			}
+		}
+	});
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks("grunt-aws-lambda");
 
-  grunt.registerTask('default', ['jshint']);
-  grunt.registerTask('deploy', ['lambda_package', 'lambda_deploy']);
+	grunt.registerTask("default", ["jshint"]);
+	grunt.registerTask("deploy", ["lambda_package", "lambda_deploy"]);
 
 };
--- index.js
+++ index.js
@@ -1,72 +1,66 @@
-require('dotenv').load();
+require("dotenv").load();
 
-var AWS = require('aws-sdk');
-var doc = require('dynamodb-doc');
-var async = require('async');
-var dynamodb = new doc.DynamoDB();
-
-actions = {
-  'handleDeployment': require('./lib/handle_deployment.js'),
-  'monitorDeployment': require('./lib/monitor_deployment.js')
+var actions = {
+	"handleDeployment": require("./lib/handle_deployment.js"),
+	"monitorDeployment": require("./lib/monitor_deployment.js")
 };
 
 var config = {
-  "opsWorksStackId": process.env.OPSWORKS_STACK_ID,
-  "opsWorksAppId": process.env.OPSWORKS_APP_ID,
-  "monitoringTopicArn": process.env.MONITORING_TOPIC_ARN,
-  "notificationTopicArn": process.env.NOTIFICATION_TOPIC_ARN,
-  "handlerTopicArn": process.env.HANDLER_TOPIC_ARN,
-  "appName": process.env.APP_NAME,
-  "lbUpstreamUser": process.env.LB_UPSTREAM_USER,
-  "lbUpstreamPassword": process.env.LB_UPSTREAM_PASSWORD,
-  "lbHost": process.env.LB_HOST,
-  "lbUpstreamPort": process.env.LB_UPSTREAM_PORT,
-  "artifactBucket": process.env.ARTIFACT_BUCKET,
-  "deploymentTimeout": process.DEPLOYMENT_TIMEOUT,
-  "overrideStatusCheck":process.env.OVERRIDE_STATUS_CHECK,
-  "submodules": [{
-    "module": "frontend",
-    "archiveName": "ui.tgz"
-  }, {
-    "module": "backend",
-    "archiveName": "engine.tgz"
-  }]
+	"opsWorksStackId": process.env.OPSWORKS_STACK_ID,
+	"opsWorksAppId": process.env.OPSWORKS_APP_ID,
+	"monitoringTopicArn": process.env.MONITORING_TOPIC_ARN,
+	"notificationTopicArn": process.env.NOTIFICATION_TOPIC_ARN,
+	"handlerTopicArn": process.env.HANDLER_TOPIC_ARN,
+	"appName": process.env.APP_NAME,
+	"lbUpstreamUser": process.env.LB_UPSTREAM_USER,
+	"lbUpstreamPassword": process.env.LB_UPSTREAM_PASSWORD,
+	"lbHost": process.env.LB_HOST,
+	"lbUpstreamPort": process.env.LB_UPSTREAM_PORT,
+	"artifactBucket": process.env.ARTIFACT_BUCKET,
+	"deploymentTimeout": process.DEPLOYMENT_TIMEOUT,
+	"overrideStatusCheck": process.env.OVERRIDE_STATUS_CHECK,
+	"submodules": [{
+		"module": "frontend",
+		"archiveName": "ui.tgz"
+	}, {
+		"module": "backend",
+		"archiveName": "engine.tgz"
+	}]
 };
 
-exports.handler = function(event, context) {
-  //console.log('Received event:', JSON.stringify(event, null, 2));
-  if (event.operation) {
-    var operation = event.operation;
-    delete event.operation;
-  } else if (event.Records[0].Sns) {
-    var message = JSON.parse(event.Records[0].Sns.Message);
-    if (!message.command) {
-      message.command = 'deploy';
-    }
-    if (!message.action && message.Records) {
-      var s3 = message.Records[0].s3;
-      if (s3) {
-        if (s3.configurationId == "ArtifactUploaded") {
-          message.action = 'handleDeployment';
-          message.command = 'prepare_staging';
-        }
-      }
-    }
-    // Skip processing monitoring call, if it's disabled in the configuration
-    if (message.action == 'monitorDeployment' && !process.env.MONITOR_ENABLED) {
-      console.log("Skipped processing message : %s", JSON.stringify(event, null, 2));
-      context.succeed();
-      return;
-    }
-    if(message.overrideStatusCheck) {
-      config.overrideStatusCheck = message.overrideStatusCheck;
-    }
+exports.handler = function (event, context) {
+	//console.log("Received event:", JSON.stringify(event, null, 2));
+	if (event.operation) {
+		delete event.operation;
+	} else if (event.Records[0].Sns) {
+		var message = JSON.parse(event.Records[0].Sns.Message);
+		if (!message.command) {
+			message.command = "deploy";
+		}
+		if (!message.action && message.Records) {
+			var s3 = message.Records[0].s3;
+			if (s3) {
+				if (s3.configurationId == "ArtifactUploaded") {
+					message.action = "handleDeployment";
+					message.command = "prepare_staging";
+				}
+			}
+		}
+		// Skip processing monitoring call, if it"s disabled in the configuration
+		if (message.action === "monitorDeployment" && !process.env.MONITOR_ENABLED) {
+			console.log("Skipped processing message : %s", JSON.stringify(event, null, 2));
+			context.succeed();
+			return;
+		}
+		if (message.overrideStatusCheck) {
+			config.overrideStatusCheck = message.overrideStatusCheck;
+		}
 
-    config.topicArn = event.Records[0].Sns.TopicArn;
-    config.messageSent = Date.parse(event.Records[0].Sns.Timestamp);
-    var targetAction = actions[message.action];
-    targetAction(message, config, context);
+		config.topicArn = event.Records[0].Sns.TopicArn;
+		config.messageSent = Date.parse(event.Records[0].Sns.Timestamp);
+		var targetAction = actions[message.action];
+		targetAction(message, config, context);
 
-    return;
-  }
+		return;
+	}
 };
